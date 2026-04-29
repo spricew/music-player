6f2cc6e38be01e38ecdec813cbc5dc921863eb46
Heyder Medina <medinaheyder@gmail.com>
2026-04-28 18:26:08 -0600

    refactor: replace PlayerBar with NavBar in MainLayout for improved structure

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,15 +1,17 @@
-import Sidebar from "@/components/layout/Sidebar"  ;
+import Sidebar from "@/components/layout/Sidebar";
 import MainContent from "./MainContent";
-import PlayerBar from "./PlayerBar";
+import NavBar from "./NavBar";
 
 export default function MainLayout() {
   return (
-    <div className="h-screen flex flex-col bg-black text-white overflow-hidden">
+    <div className="flex flex-col h-screen bg-black text-white overflow-hidden">
+      <NavBar />
+
       <div className="flex-1 flex overflow-hidden">
         <Sidebar />
         <MainContent />
       </div>
-      <PlayerBar />
+
     </div>
   );
 }
